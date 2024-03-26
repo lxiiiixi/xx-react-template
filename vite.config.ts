18a5de775ee12b207e27e7e0b0d79dd444b0cc1e
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-03-26 11:59:50 +0800

    Feat: change ts and vite config to set resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    return {
+        plugins: [react()],
+        base: "./",
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+        esbuild: {
+            drop: mode === "development" ? [] : ["console", "debugger"],
+        },
+    };
+});
